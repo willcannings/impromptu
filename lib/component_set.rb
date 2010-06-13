7c57a37d01f338f266a83b5819b7bd582bf95462
Will Cannings <me@willcannings.com>
2010-06-13 23:27:35 +1000

    WIP commit before restructuring directories

--- lib/component_set.rb
+++ lib/component_set.rb
@@ -1,13 +1,31 @@
 module Flow
   class ComponentSet
-    attr_accessor :components
+    # even though a component set can be composed of components defined in
+    # different base directories, it is assumed that the process of parsing
+    # component definition files is performed in a single thread, so
+    # setting the 'base' instance variable once per file parsed (and passing
+    # this to all new components created) should work ok.
+    attr_accessor :components, :base
 
     def initialize
       @components = {}
     end
+    
+    def parse_file(path)
+      @base = File.dirname(path)
+      File.open(path) do |file|
+        instance_eval file.read
+      end
+    end
 
     def component(name, &block)
-      @components[name] = Component.new(self, name, block)
+      if @components.has_key(name)
+        raise "Attempt to create a component using a name that already exists"
+      else
+        new_component = Component.new(@base, self, name)
+        new_component.instance_eval block
+        @components[name] = new_component
+      end
     end
   end
 end
