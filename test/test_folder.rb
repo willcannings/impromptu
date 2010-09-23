524f8a2f6e6ed83062264e10b651dc31f27de3c1
Will <me@willcannings.com>
2010-09-23 17:40:17 +1000

    Clear old tests ready for new ones

--- test/test_folder.rb
+++ test/test_folder.rb
@@ -1,32 +1,4 @@
 require 'helper'
 
 class TestFolder < Test::Unit::TestCase
-  # context "A new folder" do
-  #   setup { @folder = Impromptu::Folder.new('/folder') }
-  #   should "have no modules" do
-  #     assert_equal 0, @folder.modules.size
-  #   end
-  #   
-  #   should "have a path" do
-  #     assert_not_nil @folder.path
-  #   end
-  #   
-  #   should "respond to provides" do
-  #     assert_respond_to @folder, :provides
-  #   end
-  # 
-  #   context "with modules added" do
-  #     setup { @folder.provides(:ModuleOne, :ModuleTwo) }
-  #     should "store all modules added" do
-  #       assert_equal 2, @folder.modules.size
-  #     end
-  #     
-  #     context "and more, overlapping modules added" do
-  #       setup { @folder.provides(:ModuleThree, :ModuleOne) }
-  #       should "store all new and existing unique modules" do
-  #         assert_equal 3, @folder.modules.size
-  #       end
-  #     end
-  #   end
-  # end
 end
